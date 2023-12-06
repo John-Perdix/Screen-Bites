e650dc4a51774f45c4e477d10d743e7ea0a88c51
John-Perdix <joao_perdiz@outlook.com>
2023-12-06 13:20:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Navbar() {
+  return (
+    <div class="navbar">
+      <img alt="screenbites_logo" src="recursos/logo1.png" width="2%" height="3%"></img>
+        <h1>Receitas</h1>
+        <h1>Notícias</h1>
+        <span class="material-symbols-outlined">search</span>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
