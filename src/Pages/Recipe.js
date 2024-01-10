cae0ee7a418f646621c3affa47bbfc53ce882146
John-Perdix <joao_perdiz@outlook.com>
2024-01-10 23:47:58 +0000

    fetch fix in recipes page

--- src/Pages/Recipe.js
+++ src/Pages/Recipe.js
@@ -7,7 +7,7 @@ const Recipe = () => {
     const { id } = useParams(); //Access URL parameter
 
     const fetchRecipe = async () => {
-        await fetch('https://api.cosmicjs.com/v3/buckets/screen-bites-production/objects?pretty=true&query=%7B%22type%22:%22recipes%22%7D&limit=10&read_key=AKHFpPAIGewG9wW7Lg0XT3Q4lNOynoAFvZiCOvAuAYzY6p9yfJ&depth=1&props=slug,title,metadata,id')
+        await fetch('https://api.cosmicjs.com/v3/buckets/screen-bites-production/objects?pretty=true&query=%7B%22type%22:%22recipes%22%7D&limit=none&read_key=AKHFpPAIGewG9wW7Lg0XT3Q4lNOynoAFvZiCOvAuAYzY6p9yfJ&depth=1&props=slug,title,metadata,id')
             .then(response => response.json())
             .then(result => {
                 setRecipe(result.objects);
